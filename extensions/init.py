b086462a43be3fd9ccbed3edb226771f641fb0fe
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-05-13 22:10:28 -0700

    playing

--- extensions/init.py
+++ extensions/init.py
@@ -12,5 +12,9 @@ class GeneralMaintenance(commands.Cog):
         print(f'Logged in as {self.bot.user} (ID: {self.bot.user.id})')
         print('------')
         
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        await self.bot.change_presence(activity=discord.Game("Word Oracle, ?help"))
+        
 async def setup(bot) -> None:
     await bot.add_cog(GeneralMaintenance(bot))
